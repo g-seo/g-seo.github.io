1842ecb12161b1615af19c9d9195062e9a4c9550
Yoonjiseo <jiseo.jake@gmail.com>
2025-10-08 18:01:50 +0900

    Chnage same.

--- src/components/gitprofile.tsx
+++ src/components/gitprofile.tsx
@@ -193,8 +193,8 @@ const GitProfile = ({ config }: { config: Config }) => {
               <AvatarCard
                 profile={profile}
                 loading={loading}
-                avatarRing={sanitizedConfig.themeConfig.displayAvatarRing}
-                resumeFileUrl={sanitizedConfig.resume.fileUrl}
+                avatarRing={sanitizedConfig.themeConfig?.displayAvatarRing}
+                resumeFileUrl={sanitizedConfig.resume?.fileUrl}
               />
               <DetailsCard
                 profile={profile}
