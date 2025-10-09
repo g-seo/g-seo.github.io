93397740e8aa925a2f30f8b83409ae548575e543
Yoonjiseo <jiseo.jake@gmail.com>
2025-10-09 22:53:49 +0900

    Change div

--- src/components/gitprofile.tsx
+++ src/components/gitprofile.tsx
@@ -225,32 +225,30 @@ const GitProfile = ({ config }: { config: Config }) => {
                     educations={sanitizedConfig.educations}
                   />
                 )}
-              <div className="lg:col-span-2 col-span-1">
-                <div className="grid grid-cols-1 gap-6">
-                  {sanitizedConfig.publications.length !== 0 && (
-                    <PublicationCard
-                      loading={loading}
-                      publications={sanitizedConfig.publications}
-                    />
-                  )}
-                  {sanitizedConfig.projects.external.projects.length !== 0 && (
-                    <ExternalProjectCard
-                      loading={loading}
-                      header={sanitizedConfig.projects.external.header}
-                      externalProjects={
-                        sanitizedConfig.projects.external.projects
-                      }
-                      googleAnalyticId={sanitizedConfig.googleAnalytics.id}
-                    />
-                  )}
-                  {sanitizedConfig.blog.display && (
-                    <BlogCard
-                      loading={loading}
-                      googleAnalyticsId={sanitizedConfig.googleAnalytics.id}
-                      blog={sanitizedConfig.blog}
-                    />
-                  )}
-                </div>
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 rounded-box mb-6">
+                {sanitizedConfig.publications.length !== 0 && (
+                  <PublicationCard
+                    loading={loading}
+                    publications={sanitizedConfig.publications}
+                  />
+                )}
+                {sanitizedConfig.projects.external.projects.length !== 0 && (
+                  <ExternalProjectCard
+                    loading={loading}
+                    header={sanitizedConfig.projects.external.header}
+                    externalProjects={
+                      sanitizedConfig.projects.external.projects
+                    }
+                    googleAnalyticId={sanitizedConfig.googleAnalytics.id}
+                  />
+                )}
+                {sanitizedConfig.blog.display && (
+                  <BlogCard
+                    loading={loading}
+                    googleAnalyticsId={sanitizedConfig.googleAnalytics.id}
+                    blog={sanitizedConfig.blog}
+                  />
+                )}
               </div>
             </div>
             {sanitizedConfig.projects.github.display && (
